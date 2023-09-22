1b58e23caaf0cf437a4546014c786d72dedf75ca
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-22 16:48:29 -0400

    finished

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 window.onload = inicio;
-
-
-// function inicio() {
-//     document.querySelector("body").insertAdjacentElement("beforebegin",`
-//     <input id="campo" type="text">
-//     <button>Aceptar</button>
-//     <div id="saludo"></div>
-//     `)
-// }
+const nombreBien = "jesus";
 
 function inicio() {
     document.querySelector("body").insertAdjacentHTML("beforeend", 
-        `<input id="campo" type="text">
+        `<input id="campo" type="text"/>
         <button>Aceptar</button>
-        <div id="saludo"></div>`)
+        <div id="saludo"></div>`);
+        document.querySelector("button").addEventListener("click", leer);
 };
+
+function leer() {
+    let nombre = document.querySelector("#campo").value;
+    let nombreMayus = nombre.toUpperCase();
+    let nombreFormateado = nombre.substr(0,1).toUpperCase() + nombre.substr(1).toLowerCase();
+    if(nombreMayus === nombreBien){
+        console.log("La condicion if se cumplio")
+        document.querySelector("#saludo").innerHTML= `Hola ${nombreFormateado}`;
+    } else {
+        document.querySelector("#saludo").innerHTML = "";
+        document.querySelector("#campo").value = "";
+        document.querySelector("#campo").focus();
+    }
+}
