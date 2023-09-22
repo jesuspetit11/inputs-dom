eb396428ead2054f5457cd1f2a5a5aaad8f6e8b6
Jesus Petit <96022350+jesuspetit11@users.noreply.github.com>
2023-09-22 17:30:55 -0400

    fixed!

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = inicio;
-const nombreBien = "jesus";
+const NOMBRECORRECTO = "JESUS";
 
 function inicio() {
-    document.querySelector("body").insertAdjacentHTML("beforeend", 
+    document.querySelector("body").insertAdjacentHTML("beforeend",
         `<input id="campo" type="text"/>
         <button>Aceptar</button>
         <div id="saludo"></div>`);
@@ -13,9 +13,8 @@ function leer() {
     let nombre = document.querySelector("#campo").value;
     let nombreMayus = nombre.toUpperCase();
     let nombreFormateado = nombre.substr(0,1).toUpperCase() + nombre.substr(1).toLowerCase();
-    if(nombreMayus === nombreBien){
-        console.log("La condicion if se cumplio")
-        document.querySelector("#saludo").innerHTML= `Hola ${nombreFormateado}`;
+    if(nombreMayus == NOMBRECORRECTO){
+        document.querySelector("#saludo").innerHTML = `Hola ${nombreFormateado}`;
     } else {
         document.querySelector("#saludo").innerHTML = "";
         document.querySelector("#campo").value = "";
